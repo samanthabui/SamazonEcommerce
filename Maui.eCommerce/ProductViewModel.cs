2d0f244634c86e8a736be0f4a9cb54cd0ca16b3a
Samantha Bui <159604571+samanthabui@users.noreply.github.com>
2025-03-17 01:30:15 -0400

    Add files via upload

---
+++ Maui.eCommerce/ProductViewModel.cs
@@ -0,0 +1,50 @@
+//MAUI: CONTENT PAGE INVENTORY MANAGEMENT VIEW MODEL
+using Library.eCommerce.Services;
+using Samazon.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//BINDING MVVM: VIOLATES MVVM IF VIEW AND VIEW MODEL ARE THE SAME. THEREFORE, MAKE CLASS NAMED MAINVIEWMODEL.
+//BINDING MVVM: DECOUPLE THE DEPENDENCIES. HAVE THE ABILITY TO REUSE VIEWMODELS.
+namespace Maui.eCommerce.ViewModels
+{
+	//BINDING MVVM: PRODUCTVIEWMODEL IS TIGHTLY COUPLED TO VIEW. WANT TO ENCAPSULATE FOR THE VIEW MODEL TO HANDLE PRODUCT SERVICE PROXY.
+	//BINDING MVVM: GO TO VIEW MODEL AND CREATE A PRODUCTVIEWMODEL FUNCTION.
+	public class ProductViewModel
+	{
+		//EDIT: TYPE OF PROPERTIES - PROPERTY THAT TAKES DATA FROM CLASS, PROPERTY PUBLIC ACCESSORS TO PIECE OF INFORMATION.
+		public string? Name { 
+			get 
+			{
+				return Model?.Name ?? string.Empty;
+			}
+			set 
+			{
+				if(Model != null && Model.Name != value)
+				{
+					Model.Name = value;
+				}
+			}
+		} 
+
+		public Product? Model { get; set; }
+
+		public ProductViewModel()
+		{
+			Model = new Product();
+		}
+
+		//EDIT: CONVERSION CONSTRUCTOR CONVERTS FROM MODEL TO VIEWMODEL.
+		public ProductViewModel(Product? model)
+		{
+			Model = new Product();
+		}
+	}
+}
